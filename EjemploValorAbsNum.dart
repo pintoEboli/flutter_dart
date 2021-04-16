2a8b64366a3009a1c1a07d92dfed002b1db37d12
David Morales Calderón <david.desarrollo200@gmail.com>
2021-04-16 10:46:02 +0200

    Código Valor absoluto dart

---
+++ EjemploValorAbsNum.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  print('---------------Introduce un número---------------');
+  print('Introduce un número:');
+  // ignore: prefer_final_locals
+  int num = int.parse(stdin.readLineSync().toString());
+
+  if (num > 0) {
+    print('Numero: ($num)');
+    print('Valor Absoluto: ($num)');
+  } else {
+    //print('Numero: ($num)');
+    int valor = (num * -1);
+    print('Valor Absoluto: $valor');
+  }
+}
