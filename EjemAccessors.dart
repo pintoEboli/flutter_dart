62d2fbf6c17eb705b3a3c5fe74809d672a172d44
David Morales Calderón <david.desarrollo200@gmail.com>
2021-04-27 10:43:20 +0200

    Add method set fullName

--- EjemAccessors.dart
+++ EjemAccessors.dart
@@ -3,8 +3,16 @@ class Person {
   String lastname;
 
   Person(this.firtsname, this.lastname);
+  //Person.anonymous() {}
 
   String get fullName => "$firtsname $lastname";
+
+  set fullName(String fullName) {
+    var parts = fullName.split(" ");
+    this.firtsname = parts.first;
+    this.lastname = parts.last;
+  }
+
   String get initials => "${firtsname[0]}. ${lastname[0]}.";
 }
 
@@ -13,4 +21,6 @@ void main() {
   print(somePerson.firtsname + " " + somePerson.lastname);
   print(somePerson.fullName);
   print(somePerson.initials);
+  somePerson.fullName = "David Morales";
+  print(somePerson.fullName);
 }
